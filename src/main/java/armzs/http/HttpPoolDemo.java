45f2e0e35343a3d3dbb9cd6c79e1cdb0929b2ae7
xiangpeng <xiangpeng339@gmail.com>
2014-10-20 14:36:06 +0800

    test for retry connect

--- src/main/java/armzs/http/HttpPoolDemo.java
+++ src/main/java/armzs/http/HttpPoolDemo.java
@@ -1,14 +1,18 @@
 package armzs.http;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
+import org.apache.http.*;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.HttpRequestRetryHandler;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.conn.ConnectionKeepAliveStrategy;
 import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.protocol.HttpContext;
@@ -25,15 +29,87 @@ public class HttpPoolDemo {
 
     static {
         // 最大连接数
-        cm.setMaxTotal(10);
+        cm.setMaxTotal(2);
         // 每个路由基础的连接数
-        cm.setDefaultMaxPerRoute(10);
+        cm.setDefaultMaxPerRoute(1);
 //        HttpHost localhost = new HttpHost("localhost", 8080);
 //        cm.setMaxPerRoute(new HttpRoute(localhost), 5);
     }
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();  //获取开始时间
+//        testPool();
+        try {
+            testBlocking();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long endTime = System.currentTimeMillis(); //获取结束时间
+        System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
+    }
+
+    /**
+     * 测试对多个服务器建立连接时候的阻塞情况
+     */
+    private static void testBlocking() throws InterruptedException, IOException {
+        HttpRequestRetryHandler myRetryHandler = new HttpRequestRetryHandler() {
+
+            public boolean retryRequest(
+                    IOException exception,
+                    int executionCount,
+                    HttpContext context) {
+                if (executionCount >= 6) {
+                    // 如果已经重试了5次，就放弃
+                    return false;
+                }
+//                if (exception instanceof InterruptedIOException) {
+//                    // 超时
+//                    return false;
+//                }
+//                if (exception instanceof UnknownHostException) {
+//                    // 目标服务器不可达
+//                    return false;
+//                }
+//                if (exception instanceof ConnectTimeoutException) {
+//                    // 连接被拒绝
+//                    return false;
+//                }
+//                if (exception instanceof SSLException) {
+//                    // ssl握手异常
+//                    return false;
+//                }
+//                HttpClientContext clientContext = HttpClientContext.adapt(context);
+//                HttpRequest request = clientContext.getRequest();
+//                boolean idempotent = !(request instanceof HttpEntityEnclosingRequest);
+//                if (idempotent) {
+//                    // 如果请求是幂等的，就再次尝试
+//                    return true;
+//                }
+                return true;
+            }
+
+        };
+        CloseableHttpClient client = HttpClients.custom()
+                .setConnectionManager(cm)
+//                .setRetryHandler(myRetryHandler)
+                .build();
+        BlockThread thread1 = new BlockThread(client, "Thread1", "http://localhost:8080");
+        BlockThread thread2 = new BlockThread(client, "Thread2", "http://localhost:8080");
+        thread1.start();
+        thread1.join();
+//        Thread.sleep(15000);
+//        thread2.start();
+//        thread2.join();
+        client.close();
+    }
+
+
+    /**
+     * 测试连接池的连接复用情况
+     */
+    public static void testPool() {
 
 //        ConnectionKeepAliveStrategy myStrategy = new ConnectionKeepAliveStrategy() {
 //            @Override
@@ -66,8 +142,6 @@ public class HttpPoolDemo {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        long endTime = System.currentTimeMillis(); //获取结束时间
-        System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
     }
 }
 
@@ -84,21 +158,74 @@ class MyThread extends Thread {
     @Override
     public void run() {
         HttpGet httpGet = new HttpGet("http://localhost:8080");
-        HttpPost httpPost=new HttpPost("");
+        HttpPost httpPost = new HttpPost("");
+
         try {
-                HttpResponse response = client.execute(httpGet);
-                HttpEntity entity = response.getEntity();
-                if (entity != null) {
-                    InputStream inputStream = entity.getContent();
-                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                    byte[] buffer = new byte[1024];
-                    int size = 0;
-                    while ((size = inputStream.read(buffer)) != -1) {
-                        outputStream.write(buffer, 0, size);
-                    }
-                    System.out.print(outputStream.toString());
-                    outputStream.close();
+            HttpResponse response = client.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                InputStream inputStream = entity.getContent();
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int size = 0;
+                while ((size = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, size);
                 }
+                System.out.print(outputStream.toString());
+                outputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setShutdown() {
+        this.shutdown = true;
+    }
+}
+
+class BlockThread extends Thread {
+    private final String target;
+    private HttpClient client = null;
+
+    private volatile boolean shutdown = false;
+
+    BlockThread(HttpClient client, String theadName, String target) {
+        this.client = client;
+        this.setName(theadName);
+        this.target = target;
+    }
+
+    @Override
+    public void run() {
+        HttpPost httpPost = new HttpPost(target);
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(2000)
+                .setSocketTimeout(2000)
+                .setConnectTimeout(2000)
+                .build();//设置请求和传输超时时间
+
+        httpPost.setConfig(requestConfig);
+        httpPost.setConfig(requestConfig);
+        try {
+            HttpEntity sendEntity = new FileEntity(new File("E:\\Downloads\\ut.rar"));
+            httpPost.setEntity(sendEntity);
+            System.out.println(this.getName() + " start sending data");
+            HttpResponse response = null;
+            response = client.execute(httpPost);
+            System.out.println(this.getName() + " send end");
+            HttpEntity getEntity = response.getEntity();
+            if (getEntity != null) {
+                InputStream inputStream = getEntity.getContent();
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int size = 0;
+                while ((size = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, size);
+                }
+                System.out.print(outputStream.toString());
+                outputStream.close();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/armzs/http/HttpServerDemo.java
+++ src/main/java/armzs/http/HttpServerDemo.java
@@ -4,8 +4,12 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.apache.http.HttpException;
 
+import javax.xml.ws.http.HTTPException;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.Iterator;
@@ -26,10 +30,12 @@ public class HttpServerDemo {
     public static int threadMinCount = 3;//最小线程数
     public static int threadMaxCount = 5;//最大线程数
     public static int checkPeriod = 20;//检验时间间隔(分钟)
+    public static int listenPort = 8080;//检验时间间隔(分钟)
+
 
     public static void main(String[] args) {
 
-        InetSocketAddress addr = new InetSocketAddress(8080);
+        InetSocketAddress addr = new InetSocketAddress(listenPort);
         try {
             HttpServer server = HttpServer.create(addr, 0);
             System.out.println("Server starting ...");
@@ -39,7 +45,7 @@ public class HttpServerDemo {
                     new ThreadPoolExecutor.CallerRunsPolicy());
             server.setExecutor(threadPool);
             server.start();
-            System.out.println("Listening at 8080!");
+            System.out.println("Listening at " + listenPort);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -50,24 +56,32 @@ class MyHandler implements HttpHandler {
     private static AtomicInteger count = new AtomicInteger(0);
 
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
-        //处理请求,把请求的头部回发
+    public void handle(HttpExchange exchange) throws IOException{
+        // 处理请求
         int sequence = count.incrementAndGet();
         System.out.println(sequence);
         Headers responseHeaders = exchange.getResponseHeaders();
         responseHeaders.set("Content-Type", "text/plain");
         exchange.sendResponseHeaders(200, 0);
 
+//        ByteArrayOutputStream baos=new ByteArrayOutputStream();
+        InputStream requestBody = exchange.getRequestBody();
+        byte[] buff = new byte[1024];
+        int size = 0;
+        try {
+            while ((size = requestBody.read(buff)) != -1) {
+//                baos.write(buff,0,size);
+            }
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+//        System.out.println(baos.toString());
+//        baos.close();
+
         OutputStream responseBody = exchange.getResponseBody();
-//        Headers requestHeaders = exchange.getRequestHeaders();
-//        Set<String> keySet = requestHeaders.keySet();
-//        Iterator<String> iterator = keySet.iterator();
-//        while (iterator.hasNext()) {
-//            String key = iterator.next();
-//            List values = requestHeaders.get(key);
-//            String s = key + " = " + values.toString() + System.getProperty("line.separator");
         responseBody.write(("this is response" + sequence + System.getProperty("line.separator")).getBytes());
-//        }
         responseBody.close();
     }
 }
